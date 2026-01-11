e7a84ab7c10ce65c992c768da9162af4aa6271b5
Nv7-Github <junk4Nv7@gmail.com>
2026-01-10 16:29:53 -0800

    Bring in AdvantageKit 2026 TalonFX swerve template

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Robot2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "c2d977a11dff047598eda644d55734066e995666";
+  public static final String GIT_DATE = "2026-01-10 19:15:57 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-10 19:29:31 EST";
+  public static final long BUILD_UNIX_TIME = 1768091371372L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}

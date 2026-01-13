e096b16d3a0728b537e1c1fec301f3c9d1190cc6
Nv7-Github <junk4Nv7@gmail.com>
2026-01-12 21:47:30 -0800

    Get sim configured

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "271fcbda64717d3c5eea6686b0709798d600c07d";
-  public static final String GIT_DATE = "2026-01-12 03:37:52 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "ac7d547f4020d785258ebf6b1e12aeb4b06e317e";
+  public static final String GIT_DATE = "2026-01-13 00:42:21 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-13 00:42:13 EST";
-  public static final long BUILD_UNIX_TIME = 1768282933730L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-13 00:42:26 EST";
+  public static final long BUILD_UNIX_TIME = 1768282946921L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }

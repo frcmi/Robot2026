ce179d6d9bc356e7a57447a61179891a8d561076
0verkil <112268976+0verkil@users.noreply.github.com>
2026-01-20 23:50:28 -0800

    chore: update interplut values for new hub position

--- src/main/java/frc/robot/constants/Shooter/AimingConstants.java
+++ src/main/java/frc/robot/constants/Shooter/AimingConstants.java
@@ -7,30 +7,26 @@ public class AimingConstants {
   public static final Translation2d kHubPositionBlue = new Translation2d(4.03, 4.03); // Meters
   public static final Translation2d kHubPositionRed = new Translation2d(12.1, 4.07); // Meters
 
-  // TODO: retune for new hub position
   public static LoggedInterpolatingTable kHoodAngleTable =
       new LoggedInterpolatingTable(
           "Shooter/HoodAngleM_Deg",
           new double[][] {
             {0.0, 0.0},
-            {0.2, 5.0}, // at hub
-            {1, 15.0},
-            {1.2, 20.0},
-            {2.2, 25.0} // tower, corner
+            {0.63, 5.0}, // at hub
+            {2, 17.0}, // random data point
+            {2.7, 20.0}, // tower
+            {5.3, 30.0} // far corner
           });
 
-  // TODO: retune for new hub position
   public static LoggedInterpolatingTable kFlywheelSpeedTable =
       new LoggedInterpolatingTable(
           "Shooter/FlywheelSpeedM_RPS",
           new double[][] {
             {0.0, 0.0},
-            {0.2, 40.0}, // at hub
-            {1.0, 40.0},
-            {2.2, 44.0}, // tower
-            {2.5, 44.0},
-            {2.8, 47.0},
-            {4.8, 1000.0} // corner
+            {0.63, 40.0}, // at hub
+            {2.0, 44.0}, // random data point
+            {2.7, 47.0}, // tower
+            {5.3, 55.0} // corner
           });
   public static LoggedInterpolatingTable kAirtimeTable =
       new LoggedInterpolatingTable(

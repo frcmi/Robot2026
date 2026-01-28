77d043f9667eb65430f727e7561f11c955da6308
Nv7-Github <junk4Nv7@gmail.com>
2026-01-28 11:46:43 -0800

    Get rid of reefscape code

--- src/main/java/frc/robot/constants/AutoAlignConstants.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.constants;
-
-import edu.wpi.first.math.util.Units;
-
-public class AutoAlignConstants {
-  public static final double kAprilTagToReefDist = Units.inchesToMeters(6.469154);
-}

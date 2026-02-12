73aed24483eaa9144cfc00de7da5eeedf16e7fb7
MI-Robotics-5937 <mercerislandrobotics5937@gmail.com>
2026-02-12 15:58:13 -0800

    Changed intake voltage

--- src/main/java/frc/robot/subsystems/intake/IntakeState.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeState.java
@@ -102,7 +102,7 @@ public class IntakeState implements StructSerializable {
       new IntakeState(Degrees.of(97.5), Volts.of(0.0f), Volts.of(0.0), Volts.of(0.0), "kStowed");
   public static final IntakeState kIntaking =
       new IntakeState(
-          Degrees.of(-28.7), Volts.of(12.0f), Volts.of(6.0f), Volts.of(10.8f), "kIntaking");
+          Degrees.of(-28.7), Volts.of(12.0f), Volts.of(9.0f), Volts.of(10.8f), "kIntaking");
 
   @SuppressWarnings("unused")
   public static final Struct<IntakeState> struct =

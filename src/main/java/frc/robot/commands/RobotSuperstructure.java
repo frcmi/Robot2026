b23f8f5c4dfc2deae029f6eb17d1e32ffd0b93bd
Nv7-Github <junk4Nv7@gmail.com>
2026-02-23 13:05:35 +0530

    Weird center auto

--- src/main/java/frc/robot/commands/RobotSuperstructure.java
+++ src/main/java/frc/robot/commands/RobotSuperstructure.java
@@ -29,6 +29,7 @@ public class RobotSuperstructure {
   public void registerAutoCommands() {
     NamedCommands.registerCommand("ClimbRaise", climbRaise());
     NamedCommands.registerCommand("Climb", climbClimbed());
+    NamedCommands.registerCommand("Shoot", transfer.set(TransferState.kTransferring));
 
     new EventTrigger("Intake").whileTrue(intake.set(IntakeState.kIntaking));
     new EventTrigger("Shoot").whileTrue(transfer.set(TransferState.kTransferring));

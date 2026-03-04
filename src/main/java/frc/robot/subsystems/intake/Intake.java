2bfe1453174ee60d266fe33f8e2df3a185fe34ed
Unnat <iprobotics@gmail.com>
2026-03-03 20:43:01 -0800

    intake up when near bump

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -111,6 +111,9 @@ public class Intake extends VirtualSubsystem {
 
   @Override
   public void periodic() {
+    if(nearBump.getAsBoolean()) {
+      targetState = IntakeState.kInit;
+    }
     measuredState.setPivot(pivot.getAngle());
     measuredState.setRollers(targetState.getRollers());
 

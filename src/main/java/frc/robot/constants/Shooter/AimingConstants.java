9f32aab87df14c0862454833ea44c002d84b5656
MI-Robotics-5937 <mercerislandrobotics5937@gmail.com>
2026-03-14 16:57:20 -0700

    More changes after q29

--- src/main/java/frc/robot/constants/Shooter/AimingConstants.java
+++ src/main/java/frc/robot/constants/Shooter/AimingConstants.java
@@ -39,10 +39,10 @@ public class AimingConstants {
             {2.5, 24},
             {3, 26.3},
             {3.5, 30.5},
-            {4, 34.3},
-            {4.5, 37.5},
-            {5, 38},
-            {6, 39},
+            {4, 34},
+            {4.5, 36.5},
+            {5, 37},
+            {6, 38},
           });
   public static LoggedInterpolatingTable kAirtimeTable =
       new LoggedInterpolatingTable(

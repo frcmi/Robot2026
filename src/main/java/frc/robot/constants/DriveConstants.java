1863678c8169b50c54f1456401c845afd72f4803
Nv7-Github <junk4Nv7@gmail.com>
2026-03-17 23:33:59 -0700

    Tune path planner much better

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -6,11 +6,11 @@ public class DriveConstants {
 
   // p gain for the drive x and y controllers (side to side and forward/back)
   public static LoggedTunableNumber TRANSLATION_KP =
-      new LoggedTunableNumber("Drive/TranslationKp", 1.0);
+      new LoggedTunableNumber("Drive/TranslationKp", 12.0);
   public static LoggedTunableNumber TRANSLATION_KD =
       new LoggedTunableNumber("Drive/TranslationKd", 0.0);
-  public static LoggedTunableNumber PP_ANGLE_KP = new LoggedTunableNumber("Drive/PPAngleKp", 5.0);
-  public static LoggedTunableNumber PP_ANGLE_KD = new LoggedTunableNumber("Drive/PPAngleKd", 0.4);
+  public static LoggedTunableNumber PP_ANGLE_KP = new LoggedTunableNumber("Drive/PPAngleKp", 10.0);
+  public static LoggedTunableNumber PP_ANGLE_KD = new LoggedTunableNumber("Drive/PPAngleKd", 0.2);
 
   // p gain for the drive x and y controllers (side to side and forward/back)
   public static LoggedTunableNumber TRENCH_TRANSLATION_KP =
--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -32,7 +32,7 @@ public class TunerConstants {
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains =
-      new Slot0Configs().withKP(2.0).withKI(0).withKD(0).withKS(0.68185).withKV(0.13586);
+      new Slot0Configs().withKP(2.0).withKI(0).withKD(0).withKS(0.24745).withKV(0.75624);
   ;
 
   // The closed-loop output type to use for the steer motors;
@@ -55,7 +55,8 @@ public class TunerConstants {
 
   // The stator current at which the wheels start to slip;
   // This needs to be tuned to your individual robot
-  private static final Current kSlipCurrent = Amps.of(120);
+  private static final Current kSlipCurrent = Amps.of(60);
+  private static final Current kBackRightSlipCurrent = Amps.of(25);
 
   // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
   // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
@@ -79,7 +80,8 @@ public class TunerConstants {
 
   // Theoretical free speed (m/s) at 12 V applied output;
   // This needs to be tuned to your individual robot
-  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.12);
+  public static final LinearVelocity kSpeedAt12Volts =
+      MetersPerSecond.of(4.85); // Theoretical: 5.12m/s
 
   // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
   // This may need to be tuned to your individual robot
@@ -87,7 +89,7 @@ public class TunerConstants {
 
   private static final double kDriveGearRatio = 6.026785714285714;
   private static final double kSteerGearRatio = 26.09090909090909;
-  private static final Distance kWheelRadius = Inches.of(2);
+  private static final Distance kWheelRadius = Inches.of(1.968);
 
   private static final boolean kInvertLeftSide = false;
   private static final boolean kInvertRightSide = true;
@@ -220,15 +222,16 @@ public class TunerConstants {
           TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration>
       BackRight =
           ConstantCreator.createModuleConstants(
-              kBackRightSteerMotorId,
-              kBackRightDriveMotorId,
-              kBackRightEncoderId,
-              kBackRightEncoderOffset,
-              kBackRightXPos,
-              kBackRightYPos,
-              kInvertRightSide,
-              kBackRightSteerMotorInverted,
-              kBackRightEncoderInverted);
+                  kBackRightSteerMotorId,
+                  kBackRightDriveMotorId,
+                  kBackRightEncoderId,
+                  kBackRightEncoderOffset,
+                  kBackRightXPos,
+                  kBackRightYPos,
+                  kInvertRightSide,
+                  kBackRightSteerMotorInverted,
+                  kBackRightEncoderInverted)
+              .withSlipCurrent(kBackRightSlipCurrent);
 
   /** Swerve Drive class utilizing CTR Electronics' Phoenix 6 API with the selected device types. */
   public static class TunerSwerveDrivetrain extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder> {

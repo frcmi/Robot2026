5ebc529f085d196cd9a9104abf511dedb41afd2b
MI-Robotics-5937 <mercerislandrobotics5937@gmail.com>
2026-03-21 15:30:15 -0700

    Change far aiming a bit, make LED blue when browned out

--- src/main/java/frc/robot/constants/Shooter/AimingConstants.java
+++ src/main/java/frc/robot/constants/Shooter/AimingConstants.java
@@ -23,9 +23,9 @@ public class AimingConstants {
             {3, 28},
             {3.5, 32},
             {4, 33.8},
-            {4.5, 34.5},
-            {5, 35},
-            {6, 36},
+            {4.5, 34},
+            {5, 34.5},
+            {6, 35.5},
           });
   public static LoggedInterpolatingTable kFlywheelSpeedTable =
       new LoggedInterpolatingTable(

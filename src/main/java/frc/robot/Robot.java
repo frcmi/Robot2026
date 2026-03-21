5ad51998c9e929606300b28d1bef2a1c6a198a70
MI-Robotics-5937 <mercerislandrobotics5937@gmail.com>
2026-03-21 09:32:59 -0700

    Update at 'Sat Mar 21 09:32:59 PDT 2026'

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -59,7 +59,7 @@ public class Robot extends LoggedRobot {
           realLogPath = "/home/lvuser/logs";
         }
         Logger.addDataReceiver(new WPILOGWriter(realLogPath));
-        Logger.addDataReceiver(new NT4Publisher());
+        //Logger.addDataReceiver(new NT4Publisher()); // TODO: Bring back
         break;
 
       case SIM:

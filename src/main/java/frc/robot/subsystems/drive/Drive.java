40684ab878732734d96bfbdec4dc143b12b3c70a
Nv7-Github <junk4Nv7@gmail.com>
2026-03-21 19:17:13 -0700

    Implement TODO

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -108,7 +108,9 @@ public class Drive extends SubsystemBase {
                       && modules[1].isConnected()
                       && modules[2].isConnected()
                       && modules[3].isConnected()
-                      && gyroInputs.connected) // TODO: FIX THIS - IT SCREWS UP AUTO
+                      && (gyroInputs.connected
+                          || Constants.simMode
+                              == Constants.Mode.SIM)) // TODO: FIX THIS - IT SCREWS UP AUTO
           .debounce(0.12, DebounceType.kRising);
 
   public Drive(

b95dfcadf463624d5d26efd51b6fc73de38b56ce
Nv7-Github <junk4Nv7@gmail.com>
2026-03-22 22:50:08 -0700

    KV control

--- src/main/java/frc/robot/constants/Shooter/TurretConstants.java
+++ src/main/java/frc/robot/constants/Shooter/TurretConstants.java
@@ -64,6 +64,7 @@ public class TurretConstants {
           .kP(10.0)
           .kI(0.0)
           .kD(0.4)
+          .kV(TurretConstants.kV)
           .cruiseVelocity(RotationsPerSecond.of(5)) // 420rpm
           .acceleration(RotationsPerSecondPerSecond.of(15.0))
           .build();

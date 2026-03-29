9df9f20730f91b6d65af71861e19a31c684cc9dd
Nv7-Github <junk4Nv7@gmail.com>
2026-03-29 16:34:11 -0700

    Bump auto path?

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -43,9 +43,9 @@ public class Intake extends VirtualSubsystem implements AllianceUpdatedObserver 
   private final Timer oscillationTimer = new Timer();
 
   private final LoggedTunableNumber oscillationPeriod =
-      new LoggedTunableNumber("Intake/OscillationPeriodS", 1.2);
+      new LoggedTunableNumber("Intake/OscillationPeriodS", 0.8);
   private final LoggedTunableNumber oscillationDutyCycle =
-      new LoggedTunableNumber("Intake/OscillationDutyCycle", 0.6);
+      new LoggedTunableNumber("Intake/OscillationDutyCycle", 0.8);
   private final LoggedTunableNumber oscillationInitialDelay =
       new LoggedTunableNumber("Intake/OscillationInitialDelay", 0.5);
 

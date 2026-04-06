dea68fb41348f33b231d1a1f857052d9ebe97de9
Unnat <iprobotics@gmail.com>
2026-04-05 20:40:16 -0700

    clamp dynamic dt, also claude code is not smart enough for genuine ideation lol

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -322,7 +322,7 @@ public class Drive extends SubsystemBase {
     Logger.recordOutput("SwerveStates/Speeds", speeds);
 
     // Calculate module setpoints
-    ChassisSpeeds discreteSpeeds = ChassisSpeeds.discretize(speeds, periodicDt);
+    ChassisSpeeds discreteSpeeds = ChassisSpeeds.discretize(speeds, Math.min(periodicDt, 0.05));
     SwerveModuleState[] setpointStates = kinematics.toSwerveModuleStates(discreteSpeeds);
     SwerveDriveKinematics.desaturateWheelSpeeds(setpointStates, TunerConstants.kSpeedAt12Volts);
 

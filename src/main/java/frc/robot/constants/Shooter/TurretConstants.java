67ed43e151b99892fde266db5d6963b79b723dfc
MI-Robotics-5937 <mercerislandrobotics5937@gmail.com>
2026-05-01 10:31:47 -0700

    Update at 'Fri May 01 10:31:47 PDT 2026'

--- src/main/java/frc/robot/constants/Shooter/TurretConstants.java
+++ src/main/java/frc/robot/constants/Shooter/TurretConstants.java
@@ -38,7 +38,7 @@ public class TurretConstants {
   public static final Translation3d TurretCameraOffset =
       new Translation3d(Inches.of(7.005).in(Meters), 0, Inches.of(18.010477).in(Meters));
   public static final Rotation3d TurretCameraRotation =
-      new Rotation3d(0, Math.toRadians(150), Math.toRadians(180));
+      new Rotation3d(0, Math.toRadians(144), Math.toRadians(180)); // 32 deg pitch
 
   public static final Angle kTurretPhysicalMinAngle = Degrees.of(-235);
   public static final Angle kTurretPhysicalMaxAngle =
